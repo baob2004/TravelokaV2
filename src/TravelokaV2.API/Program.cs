9b91415c40490155751467a647f36607ef1115a4
DinhQuocBao <baob2004@gmail.com>
2025-10-08 12:48:40 +0700

    map controller and add scalar

--- src/TravelokaV2.API/Program.cs
+++ src/TravelokaV2.API/Program.cs
@@ -1,8 +1,10 @@
+using Scalar.AspNetCore;
 using TravelokaV2.Application;
 using TravelokaV2.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
@@ -12,9 +14,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
 
