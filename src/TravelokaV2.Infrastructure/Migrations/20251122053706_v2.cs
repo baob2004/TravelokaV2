b294527bdfe58aa43d32c18955e59cc07e7b415a
thanh <thanh@DATDEV>
2025-11-22 14:10:45 +0700

    final models

---
+++ src/TravelokaV2.Infrastructure/Migrations/20251122053706_v2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelokaV2.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "AspNetUsers");
+        }
+    }
+}
